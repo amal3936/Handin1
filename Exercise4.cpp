1bc799dc18c291f7aa89430b047f172f9d4bfcad
Ehabben10 <202107666@post.au.dk>
2024-02-08 12:14:27 +0100

    ex 4

--- Exercise4.cpp
+++ Exercise4.cpp
@@ -1,43 +1,59 @@
 #include <vector>
-#include <algorithm>
-
-class MaxHeap : public std::vector<int>
+class MaxHeap
 {
+private:
+    std::vector<int> heap;
+
 public:
-    // Constructor
-    MaxHeap() {}
-
     // is the heap empty?
-    bool isEmpty() const override
+    bool isEmpty() const
     {
-        return empty();
+        if (heap.size() == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
-
     // number of elements in the heap
-    int size() override
+    int size() const
     {
-        return static_cast<int>(this->size());
+        return heap.size();
     }
-
     // add an element to the heap
-    void insert(const int x) override
+    void insert(const int x)
     {
-        push_back(x);
-        std::push_heap(begin(), end());
+        heap.push_back(x);
     }
-
     // find the maximum element in the heap
-    const int findMax() const override
+    const int findMax() const
     {
-        return front();
+        int max = heap[0];
+        for (int i = 0; i < heap.size(); i++)
+        {
+            if (heap[i] > max)
+            {
+                max = heap[i];
+            }
+        }
+        return max;
     }
-
     // delete and return the maximum element of the heap
-    int deleteMax() override
+    virtual int deleteMax()
     {
-        int maxElement = front();
-        std::pop_heap(begin(), end());
-        pop_back();
-        return maxElement;
+        int max = heap[0];
+        int index = 0;
+        for (int i = 0; i < heap.size(); i++)
+        {
+            if (heap[i] > max)
+            {
+                max = heap[i];
+                index = i;
+            }
+        }
+        heap.erase(heap.begin() + index);
+        return max;
     }
 };
